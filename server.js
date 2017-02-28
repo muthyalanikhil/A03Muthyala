255ae269c84faee67684247898134d39472f5ed2
nikhil <S528754@mail.nwmissouri.edu>
2017-02-27 22:00:28 -0600

    NM: updated files with new paths

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 var express = require('express');
 var app = express(); // function handler
 //var http = require('http').createServer(app);  // http server
-
+app.use(express.static(__dirname + '/assets'));
 // Initialize app with route / (the root) "on getting a request to /, do the following"
 app.get('/', function (req, res) {
-  res.write('This is where we will show our chat client.\n');
-  res.write('We need an element to hold messages.\n');
-  res.write('We need an element to hold notification (that others are typing).\n');
-  res.write('We need an element to hold the message input form.\n');
-  res.end();
+  res.sendfile('assets/home.html');
 });
 
 // Listen for an application request on port 8081
